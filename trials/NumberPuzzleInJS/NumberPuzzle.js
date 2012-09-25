a61c98e3ad148c066f8e5baf76aeb268f84e80ed
Varun K <aawc@googlegroups.com>
2012-09-25 10:56:25 -0700

    First shot at implementing the numbers puzzle in JavaScript
    Still a work in progress.

---
+++ trials/NumberPuzzleInJS/NumberPuzzle.js
@@ -0,0 +1,71 @@
+var Board = function (numRows) {
+	this.rows = numRows;
+	this.cols = numRows;
+	this.maxSlots = this.rows * this.cols;	// 16
+	this.maxTiles = this.maxSlots - 1;		// 15
+	
+	this.finalPosition = [];
+	this.generateFinalPosition = function () {
+		var maxNumber = this.rows * this.cols;
+		for (var i = 0; i < this.maxTiles; i++) {
+			this.finalPosition[i] = i;
+		}
+		console.log(this.finalPosition);
+	};
+	
+	this.slotInWhichTileIsPlaced = [];	// Slot in which tile 'i' is placed.
+	this.tileInEachSlot = [];	// Tile in slot 'i'.
+	this.generateTileLocation = function () {
+		this.slotInWhichTileIsPlaced = [];
+		this.tileInEachSlot = [];
+		while (this.slotInWhichTileIsPlaced.length < this.maxTiles) {
+			var position = Math.floor(Math.random() * this.maxTiles);
+			if (this.slotInWhichTileIsPlaced.indexOf(position) === -1) {
+				this.slotInWhichTileIsPlaced.push(position);
+				console.log(this.slotInWhichTileIsPlaced);
+			} else {
+				// console.log("Rejected: " + position);
+			}
+		}
+		
+		var isDifferent = false;
+		for (var i = 0; i < this.maxTiles; i++) {
+			if (this.slotInWhichTileIsPlaced[i] !== this.finalPosition[i]) {
+				isDifferent = true;
+			}
+		}
+		if (!isDifferent) {
+			this.generateTileLocation();
+		} else {
+			for (var i = 0; i < this.maxTiles; i++) {
+				this.tileInEachSlot.push(this.slotInWhichTileIsPlaced.indexOf(i) + 1);
+			}
+			console.log(this.tileInEachSlot);
+		}
+	};
+	
+	this.displayTileLocation = function (array) {
+		var s = "";
+		for (var i = 0; i < this.rows; i++) {
+			for (var j = 0; j < this.cols; j++) {
+				var value = array[i * this.rows + j];
+				if (value !== undefined) {
+					s += value;
+				} else {
+					s += "_";
+				}
+				s += "\t";
+			}
+			s += "\n";
+		}
+		
+		console.log(s);
+	};
+	
+	this.generateFinalPosition();
+	this.generateTileLocation();
+	this.displayTileLocation(this.tileInEachSlot);
+	this.displayTileLocation(this.finalPosition);
+};
+
+var board = new Board (3);
