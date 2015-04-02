71b89f21ff37190266637ea3590e3466876df8b0
Varun Khaneja <ca.aawc@yahoo.com>
2015-04-01 20:39:22 -0700

    Remove explicit unicode handling

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -2,9 +2,6 @@
 # Skeleton file for the Python "Bob" exercise.
 #
 
-import re
-import unicodedata
-
 def hey(what):
     what = what.strip()
     if not what:
@@ -14,17 +11,8 @@ def hey(what):
     for abbr in known_abbrv:
         what = what.replace(abbr, '')
 
-    what = remove_accents(what)
-    words = re.findall('\w+', what)
-    for word in words:
-        if word.isupper():
-            return 'Whoa, chill out!'
-
-    if what[-1] == '?':
+    if unicode(what).isupper():
+        return 'Whoa, chill out!'
+    elif what[-1] == '?':
         return 'Sure.'
     return 'Whatever.'
-
-
-def remove_accents(input_str):
-    nkfd_form = unicodedata.normalize('NFKD', input_str)
-    return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
