840df83a5f0413e77404281cc564d7f52b6ccf0c
Varun Khaneja <ca.aawc@yahoo.com>
2016-07-11 14:28:45 -0700

    make all, clean. string substring with null embedded.

--- cpp/string.cc
+++ cpp/string.cc
@@ -14,5 +14,13 @@ int main()
   lastname.push_back('\0');
   std::cout << "Hello, " << firstname << " " << lastname << "!\n";
   std::cout << lastname.length() << "!\n";
+
+  lastname.push_back('G');
+  std::cout << "Hello, " << firstname << " " << lastname << "!\n";
+  std::cout << lastname.length() << "!\n";
+
+  std::string::iterator iter = lastname.begin();
+  std::string partial_lastname(iter+5, iter+8);
+  std::cout << "Hello, " << partial_lastname << "!\n";
 }
 
